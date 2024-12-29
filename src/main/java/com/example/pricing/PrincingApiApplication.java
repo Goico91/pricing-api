ce4b1088c91afe7ac7d92e23ab4aa5c55574d038
Carlos Goicoechea <cgoico91@gmail.com>
2024-12-29 16:26:45 +0100

    Initial Commit

---
+++ src/main/java/com/example/pricing/PrincingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrincingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrincingApiApplication.class, args);
+	}
+
+}
